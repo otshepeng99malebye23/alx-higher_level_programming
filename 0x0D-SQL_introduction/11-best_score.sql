3fc694b48cc8e95efe4323b47cf57d72ccee8306
Otshepeng <otshepengmalebye@gmail.com>
2023-10-19 15:37:36 -0700

    1

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,3 @@
+
+-- show all records from table by score only scores greated than 9
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,3 @@
+
+-- show all rows
+SELECT * FROM first_table;
